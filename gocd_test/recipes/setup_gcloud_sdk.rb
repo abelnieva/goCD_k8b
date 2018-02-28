71e7cc2c058ec465339b1bb8739aa80841dac278
Abel <abel@localhost.localdomain>
2018-02-28 03:57:55 -0300

    fixing erros

--- gocd_test/recipes/setup_gcloud_sdk.rb
+++ gocd_test/recipes/setup_gcloud_sdk.rb
@@ -3,6 +3,7 @@ bash 'installgcloud' do
    wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-190.0.1-linux-x86_64.tar.gz
    tar -xvzf google-cloud-sdk-190.0.1-linux-x86_64.tar.gz
    ./google-cloud-sdk/install.sh --usage-reporting=false --quiet
+   sudo chown -R chefuser /home/chefuser/.config/
     EOH
 end
 include_recipe 'kubectl::default'
